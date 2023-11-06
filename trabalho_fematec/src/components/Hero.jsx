935b79304770c726a13b232314f73f0d42b55a12
Lucas Nunes Carvalho <lucasnc10@hotmail.com>
2023-11-05 21:33:54 -0300

    Inclusão da lógica do efeito Typed na HERO.JS

--- trabalho_fematec/src/components/Hero.jsx
+++ trabalho_fematec/src/components/Hero.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import lamp from "../assets/Lamp.gif";
 import { useNavigate } from 'react-router-dom';
+import {useTypewriter} from "react-simple-typewriter";
 
 const Hero = () => {
 
@@ -10,6 +11,11 @@ const Hero = () => {
         navigate('/calculator');
     };
 
+    const [text] =useTypewriter({
+        words: ["LED", "FLUORESCENTE", "INCANDESCENTE"],
+        loop: {}
+    })
+
     return (
         <div className="text-white bg-no-repeat bg-right-bottom" style={{ backgroundImage: `url("${lamp}")` }}>
             <div className="max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center ">
@@ -17,6 +23,9 @@ const Hero = () => {
                 <h1 className="md: text-7xl sm:text-6xl font-bold m:py-6">Não faça a escolha errada.</h1>
                 <div>
                     <p className="md:text-5xl sm:text-4xl text-xl font-bold">Entenda suas opções</p>
+                    <div className="md: text-5xl sm:text-4xl font-bold md:pl-4 pl-2 text-[#00df9a]">
+                        {text}
+                    </div>
                     <div>
                         <p className="md:text-2xl text-xl font-bold text-gray-500">Monitore seus custos aqui.</p>
                         <button onClick={handleNavigateToCalculator} className="bg-[#00df9a] w-[200px] rounded-full font-medium my-6 mx-auto py-3 text-black" >Começar</button>
